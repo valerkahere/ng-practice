10f50db950ceb906214e6e749afc1f829b87822f
Valerii Navalnyi <adultvalerka@gmail.com>
2026-01-30 22:51:10 +0000

    Display components in the main component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './components/header/header';
+import { Home } from './home/home';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home],
   template: `
+    <app-header></app-header>
+    <app-home></app-home>
+
     <h1>Hello, {{ title() }}</h1>
     <h2>brugh</h2>
     <router-outlet />
