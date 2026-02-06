2155f57e2c206d5200d96e13ff30009345d5873b
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-06 17:23:21 +0000

    Add greeting component

---
+++ src/app/components/greeting/greeting.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.html',
+  styleUrl: './greeting.scss',
+})
+export class Greeting {
+
+}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Greeting } from '../components/greeting/greeting';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Greeting],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
