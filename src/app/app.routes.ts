a2a82a11ee385d094f9aa4b7556b1aebca424421
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-13 13:53:16 +0000

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// if path is empty, which means we're at the top or ROOT
+// (pathMatch full required if path is empty)
+// load 
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import("./home/home").then(
+                m => m.Home
+            )
+        }
+    }, 
+    {
+        path: 'todos',
+        loadComponent: () => {
+            return import("./todos/todos").then(
+                m => m.Todos
+            )
+        }
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Header } from './components/header/header';
-import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header],
   template: `
     <app-header></app-header>
     <main>
-        <app-home></app-home>
     </main>
     
 
