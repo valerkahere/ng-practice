861b4e5418719e785f4b0b04d0f24c30609f485f
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-13 12:23:19 +0000

    improve counter

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -7,10 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './counter.scss',
 })
 export class Counter {
-    clickHandler() {
+    // find out which button was clicked
+    clickHandler(event: Event) {
+        // This is the most reliable way because it always refers to the element that the event listener is attached to, even if you clicked an icon inside the button. 
+        
         let countEl = document.getElementById("count")
-        if (countEl !== null && countEl !== undefined) {
-            countEl.innerHTML = (Number(countEl.innerHTML) + 1).toString();
+        if (countEl !== null) {
+            
+
+            if (event.currentTarget !== null) {
+                // if the target is reset button, reset the count
+                if (event.currentTarget === document.getElementById("btnReset"))
+                {
+                    countEl.innerHTML = "0"; // must be a string
+                } else { // if the target is increment button, then increment
+                    countEl.innerHTML = (Number(countEl.innerHTML) + 1).toString();
+                }
+                
+            }
+            
         }
+
+        
     }
 }
