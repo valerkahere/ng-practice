c530ec9587b14d0efa296d30f2462b7e2db412cb
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-13 15:30:29 +0000

    servicd

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,6 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodoItem } from '../components/todo-item/todo-item';
 import { TodosService } from '../services/todos/todos-service';
+import { Todo } from '../model/todo.type';
 
 @Component({
   selector: 'app-todos',
@@ -11,8 +12,11 @@ import { TodosService } from '../services/todos/todos-service';
 export class Todos implements OnInit {
     todoService = inject(TodosService);
 
+    todoItems = signal<Array<Todo>>([]);
+
     ngOnInit(): void {
         console.log(this.todoService.todoItems);
+        this.todoItems.set(this.todoService.todoItems);
     }
 
 }
