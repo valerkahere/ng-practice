5dc7a07866d7ab2dec24664e3e98022b5e9bfbcc
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-13 14:48:31 +0000

    render todos

---
+++ src/app/components/todo-item/todo-item.ts
@@ -0,0 +1,12 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  imports: [],
+  templateUrl: './todo-item.html',
+  styleUrl: './todo-item.scss',
+})
+export class TodoItem {
+    
+    
+}
--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TodoItem } from '../components/todo-item/todo-item';
+import { TodosService } from '../services/todos/todos-service';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItem, ],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
-export class Todos {
+export class Todos implements OnInit {
+    todoService = inject(TodosService);
+
+    ngOnInit(): void {
+        console.log(this.todoService.todoItems);
+    }
 
 }
